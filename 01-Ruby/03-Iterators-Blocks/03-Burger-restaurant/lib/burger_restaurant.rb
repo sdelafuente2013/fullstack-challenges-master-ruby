f7a6a764a0f763f850635bfd390540075778ecfc
santi <santiagodelafuente2013@gmail.com>
2022-01-13 20:40:16 -0300

    Ejercicio completo

--- 01-Ruby/03-Iterators-Blocks/03-Burger-restaurant/lib/burger_restaurant.rb
+++ 01-Ruby/03-Iterators-Blocks/03-Burger-restaurant/lib/burger_restaurant.rb
@@ -1,3 +1,8 @@
 def burger(patty, sauce, topping)
-  # TODO: code the `burger` method
+  # TODO: code the `burger` method3
+  if block_given?
+    return ["bread", yield(patty), sauce, topping, "bread"]
+  else
+    return ["bread", patty, sauce, topping, "bread"]
+  end
 end
