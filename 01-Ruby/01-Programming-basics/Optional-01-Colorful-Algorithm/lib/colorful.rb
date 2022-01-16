d9c0ae137fa2444c321ca61be6301a79fc55429a
santi <santiagodelafuente2013@gmail.com>
2022-01-16 14:28:34 -0300

    Ejercicio 01 opcional terminado

--- 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
+++ 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
@@ -1,4 +1,21 @@
 def colorful?(number)
   # TODO: return true if the number is colorful, false otherwise
-end
+  new_array = number.to_s.chars.map do |num|
+    num.to_i
+  end
 
+  case new_array.length
+  when 2
+    new_array << (new_array[0] * new_array[1])
+  when 3
+    new_array << (new_array[0] * new_array[1])
+    new_array << (new_array[1] * new_array[2])
+    new_array << (new_array[0] * new_array[1] * new_array[2])
+  end
+
+  if (new_array.length - new_array.uniq.length).zero?
+    return true
+  else
+    return false
+  end
+end
