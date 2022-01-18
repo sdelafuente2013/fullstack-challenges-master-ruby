9f811e2af6d1af02c1b17228d2080783ad32c66a
santi <santiagodelafuente2013@gmail.com>
2022-01-18 02:27:38 -0300

    Ejercicio 01 Terminado

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
@@ -1,3 +1,15 @@
 def poor_calories_counter(burger, side, beverage)
   # TODO: return number of calories for this mcDonald order
+  return CALORIES[burger] + CALORIES[side] + CALORIES[beverage]
 end
+
+CALORIES = {
+  "Hamburger" => 250,
+  "Cheese Burger" => 300,
+  "Big Mac" =>	540,
+  "McChicken" => 350,
+  "French Fries" =>	230,
+  "Salad" => 15,
+  "Coca Cola" => 150,
+  "Sprite" => 150
+}
