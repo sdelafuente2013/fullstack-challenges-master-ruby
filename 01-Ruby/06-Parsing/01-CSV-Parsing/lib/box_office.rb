1acb501df6a7fdcd3d3cb137b2785b50436f87ef
santi <santiagodelafuente2013@gmail.com>
2022-01-19 13:04:41 -0300

    Terminando ejercicios..

--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -1,5 +1,16 @@
 require 'csv'
 
+# file_path = "movies.csv"
+
 def most_successful(number, max_year, file_path)
-  # TODO: return the `number` of most successful movies before `max_year`
+  film_finded = []
+  CSV.foreach(file_path).each_with_index do |row, index|
+    if (row[1].to_i < max_year) && (film_finded.length < number)
+      film_finded[index] = { name: row[0], year: row[1].to_i, earnings: row[2] }
+    else
+      return film_finded
+    end
+  end
 end
+
+# p most_successful(2, 2009, file_path)
--- 01-Ruby/06-Parsing/Reboot-01-Christmas-list/lib/interface.rb
+++ 01-Ruby/06-Parsing/Reboot-01-Christmas-list/lib/interface.rb
@@ -1,1 +1,13 @@
-# TODO: you can build your christmas list program here!
+puts "Welcome to your Christmas gift list"
+puts Which action [list|add|delete|quit]?
+> list
+> TODO: list items
+> Which action [list|add|delete|quit]?
+> add
+> TODO: ask user an item and add it to gift list
+> Which action [list|add|delete|quit]?
+> delete
+> TODO: ask user the index of item to delete and delete it
+> Which action [list|add|delete|quit]?
+> quit
+> Goodbye
