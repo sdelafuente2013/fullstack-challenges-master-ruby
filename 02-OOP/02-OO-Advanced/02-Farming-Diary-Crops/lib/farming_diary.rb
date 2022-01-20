c5ad08a89d848c3587f530e1a887506fab8d4419
santi <santiagodelafuente2013@gmail.com>
2022-01-20 13:29:19 -0300

    Ejercicio 02 Terminado

--- 02-OOP/02-OO-Advanced/02-Farming-Diary-Crops/lib/farming_diary.rb
+++ 02-OOP/02-OO-Advanced/02-Farming-Diary-Crops/lib/farming_diary.rb
@@ -7,27 +7,27 @@ require_relative "rice"
 
 puts "\n\n📝 Day One: Corn"
 # 1. Instantiate a corn crop
-
+corn_uno = Corn.new
 # 2. Water the corn crop
-
+corn_uno.water!
 # 3. Replace 'TODO' with the value of `grains` for your crop
-puts "The corn crop produced #{'TODO'} grains"
+puts "The corn crop produced #{corn_uno.grains} grains"
 # 4. Replace 'TODO' with the state of the crop ('ripe' or 'not ripe')
-puts "The corn crop is #{'TODO'}"
+puts "The corn crop is #{corn_uno.ripe?}"
 
 
 
 puts "\n\n📝 Day Two: Rice"
 # 1. Instantiate a rice crop
-
+rice_uno = Rice.new
 # 2. Water the rice crop
-
+rice_uno.water!
 # 3. Transplant the rice crop
-
+rice_uno.transplant!
 # 4. Replace 'TODO' with the value of `grains` for your crop
-puts "The rice crop produced #{'TODO'} grains"
+puts "The rice crop produced #{rice_uno.grains} grains"
 # 5. Replace 'TODO' with the state of the crop ('ripe' or 'not ripe')
-puts "The rice crop is #{'TODO'}"
+puts "The rice crop is #{rice_uno.ripe?}"
 
 
 # DO NOT remove the two lines below, needed for display purposes
