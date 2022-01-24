a89a9ae4ab540ac23b491476510f766ac79a8c61
santi <santiagodelafuente2013@gmail.com>
2022-01-24 16:06:09 -0300

    Ejercicio 01 terminado

--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -1,16 +1,14 @@
 require 'csv'
 
-# file_path = "movies.csv"
-
 def most_successful(number, max_year, file_path)
   film_finded = []
-  CSV.foreach(file_path).each_with_index do |row, index|
-    if (row[1].to_i < max_year) && (film_finded.length < number)
-      film_finded[index] = { name: row[0], year: row[1].to_i, earnings: row[2] }
-    else
-      return film_finded
-    end
-  end
-end
 
-# p most_successful(2, 2009, file_path)
+  # Agregamos la cantidad de peliculas que son menores al año elegido
+  CSV.foreach(file_path) do |row|
+    film_finded << { name: row[0], year: row[1].to_i, earnings: row[2].to_i } if row[1].to_i < max_year
+  end
+  # Filtramos por las que tienen mas ganancias (la cantidad la elige el usuario mediante: number)
+  film_finded = film_finded.max_by(number) { |movie| movie[:earnings] }
+
+  return film_finded
+end
