4f4e7a354e0fcaa864072ca7e8c5806e0d816979
santi <santiagodelafuente2013@gmail.com>
2022-01-31 13:33:51 -0300

    Ejercicio 03 terminado

--- 03-AR-Database/03-ActiveRecord-Basics/02-Migrations/db/migrate/20141025152200_create_posts.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/02-Migrations/db/migrate/20141025152200_create_posts.rb
@@ -1,5 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
-    # TODO: your code here to create the posts table
+    create_table :posts do |t|
+      t.string  :title
+      t.string  :url
+      t.timestamps
+    end
   end
 end
