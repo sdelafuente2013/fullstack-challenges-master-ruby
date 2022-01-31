fa14169f9589f4cdd95992aea9e2c19b2923303a
santi <santiagodelafuente2013@gmail.com>
2022-01-30 21:47:20 -0300

    Ejercicio 02 terminado

--- 03-AR-Database/02-SQL-CRUD/02-Read/app/models/post.rb
+++ 03-AR-Database/02-SQL-CRUD/02-Read/app/models/post.rb
@@ -3,5 +3,26 @@
 # NO NEED TO CREATE IT, JUST USE IT.
 
 class Post
-  # TODO
+  attr_reader :votes, :id
+  attr_accessor :title, :url
+
+  def initialize(attributes = {})
+    @id = attributes[:id]
+    @title = attributes[:title]
+    @url = attributes[:url]
+    @votes = attributes[:votes]
+  end
+
+  def self.find(id)
+    DB.results_as_hash = true
+    post = DB.execute("SELECT * FROM posts WHERE id == ?", id).first
+    Post.new(id: post["id"], title: post["title"], url: post["url"], votes: post["votes"]) unless post.nil?
+  end
+
+  def self.all
+    DB.results_as_hash = true
+    DB.execute("SELECT * FROM posts").map do |post|
+      Post.new(id: post["id"], title: post["title"], url: post["url"], votes: post["votes"])
+    end
+  end
 end
