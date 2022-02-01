0b1fa815c249f7578a0ddcf8361caf557d3b99f3
santi <santiagodelafuente2013@gmail.com>
2022-02-01 17:39:23 -0300

    Ejercicio 03 terminado

--- 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/controllers/posts_controller.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 require_relative "../views/posts_view"
+# require_relative "../models/post"
+# require 'active_record'
+# require 'sqlite3'
 
 class PostsController
   def initialize
@@ -9,25 +12,40 @@ class PostsController
     # DO NOT WRITE SQL QUERIES
     # TODO: gather all posts from the database
     # TODO: give them to the view to be printed
+    @view.display_post(Post.all)
   end
 
   def create
     # DO NOT WRITE SQL QUERIES
     # TODO: create a post
+    @view.answers_user
+    Post.create(title: @view.title, url: @view.url, votes: @view.votes)
   end
 
   def update
     # DO NOT WRITE SQL QUERIES
     # TODO: update a post
+    @view.answers_user_update
+    user_update = Post.find_by(title: @view.name_find)
+    user_update.update(title: @view.name_update)
   end
 
   def destroy
     # DO NOT WRITE SQL QUERIES
     # TODO: destroy a post
+    user_destroy = @view.answers_user_destroy
+    if user_destroy.is_a?(String)
+      Post.find_by(title: user_destroy).destroy
+    else
+      Post.find_by(id: user_destroy).destroy
+    end
   end
 
   def upvote
     # DO NOT WRITE SQL QUERIES
     # TODO: increment the `votes` column for a post
+    user_upvote = @view.answers_user_upvote
+    user = Post.find_by(title: user_upvote)
+    user.update(votes: user.votes + 1)
   end
 end
--- 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/views/posts_view.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/views/posts_view.rb
@@ -1,3 +1,44 @@
 class PostsView
   # TODO: implement some methods here when controller needs to `puts` or `gets`
+  attr_reader :title, :url, :votes, :name_find, :name_update, :url_update
+
+  def initialize(attributes = {})
+    @title = attributes[:title]
+    @url = attributes[:url]
+    @votes = 0
+    @name_update = ""
+    @url_update = ""
+    @name_find = ""
+  end
+
+  def display_post(obj_active_record)
+    obj_active_record.each do |post|
+      puts "#{post.id}. #{post.title} - #{post.votes} votes - #{post.url}"
+    end
+  end
+
+  def answers_user
+    puts "Muchas gracias por elegir crear un post, a continuacion le haremos unas pregunas..."
+    puts "¿Que titulo le gustaria añadir?"
+    @title = gets.chomp
+    puts "Indiquenos la URL:"
+    @url = gets.chomp
+  end
+
+  def answers_user_update
+    puts "¿Que titulo le gustaria cambiar?"
+    @name_find = gets.chomp
+    puts "Escriba el nuevo nombre del titulo:"
+    @name_update = gets.chomp
+  end
+
+  def answers_user_destroy
+    puts "¿Que title o ID desea eliminar?"
+    gets.chomp
+  end
+
+  def answers_user_upvote
+    puts "¿A que titulo le gustaria votar?"
+    gets.chomp
+  end
 end
