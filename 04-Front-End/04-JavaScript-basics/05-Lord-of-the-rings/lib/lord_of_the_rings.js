1560e056321a8196ac03c9599acc913c98912d51
santi <santiagodelafuente2013@gmail.com>
2022-02-07 17:19:12 -0300

    Ejercicio 04 terminado

--- 04-Front-End/04-JavaScript-basics/05-Lord-of-the-rings/lib/lord_of_the_rings.js
+++ 04-Front-End/04-JavaScript-basics/05-Lord-of-the-rings/lib/lord_of_the_rings.js
@@ -1,14 +1,37 @@
 const isGood = (soldierType) => {
   // TODO: return `true` if the soldierType is Good.
   // Hint: Hobbits, Elves, Dwarves and Eagles are Good
+  const goods = ['Hobbits', 'Elves', 'Dwarves', 'Eagles'];
+  return goods.includes(soldierType);
 };
 
 const buildSoldierObject = (battlefield) => {
   // TODO: Given a battlefield (String), return an object of forces.
+  const newObject = {};
+  battlefield.split(",").forEach((hero) => {
+    newObject[hero.split(':')[0]] = Number.parseInt(hero.split(':')[1], 10);
+  });
+  return newObject;
 };
 
 const whoWinsTheWar = (battlefield) => {
-  // TODO: Based on the battlefield's description (it's a String), return "Good", "Evil" or "Tie".
+  let pointGoods = 0;
+  let pointBad = 0;
+  battlefield.split(",").forEach((hero) => {
+    if (isGood(hero.split(":")[0])) {
+      pointGoods += Number.parseInt(hero.split(":")[1], 10);
+    } else {
+      pointBad += Number.parseInt(hero.split(":")[1], 10);
+    }
+  });
+  if (pointGoods > pointBad) {
+    return 'Good';
+  }
+  if (pointBad > pointGoods) {
+    return 'Evil';
+  }
+  return 'Tie';
 };
+
 
 module.exports = { whoWinsTheWar, buildSoldierObject, isGood }; // Do not remove this line
