7e5ee078041da5960954b5fa3ff0203a0c80a4ce
santi <santiagodelafuente2013@gmail.com>
2022-02-08 13:25:39 -0300

    Ejercicio 03 completado

--- 04-Front-End/05-DOM-and-Events/03-My-First-Event-Listener/lib/listener.js
+++ 04-Front-End/05-DOM-and-Events/03-My-First-Event-Listener/lib/listener.js
@@ -1,1 +1,8 @@
 // TODO: React to a click on the button!
+const audio = new Audio('../sound.mp3');
+
+document.querySelector("#clickme").addEventListener('click', (e) => {
+  e.currentTarget.classList.add('disabled');
+  e.currentTarget.innerText = 'Bingo!';
+  audio.play();
+});
