545f93580fd1dfde6651d965bfeac1cc74644ce8
santi <santiagodelafuente2013@gmail.com>
2022-02-08 18:15:34 -0300

    Ejercicio 05 terminado

--- 04-Front-End/05-DOM-and-Events/05-Fake-Inbox/lib/inbox.js
+++ 04-Front-End/05-DOM-and-Events/05-Fake-Inbox/lib/inbox.js
@@ -1,34 +1,43 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable no-multiple-empty-lines */
 
 const hasNewMessage = () => {
   // TODO: return true with a probability of 20%.
+  const random = Math.floor(Math.random() * (10 - 0));
+  if (random < 2) {
+    return true;
+  } return false;
 };
 
 const newMessage = () => {
   // TODO: return a random message as an object with two keys, subject and sender
+  const objetRandom = [{ sender: 'Arnoldss Chalustra', subject: "I need to speak with you!!!" },
+    { sender: 'Arnold Schwarzenegger', subject: "I'm back" }];
+  const random = Math.floor(Math.random() * (2 - 0));
+  return objetRandom[random];
 };
 
 const appendMessageToDom = (message) => {
   // TODO: append the given message to the DOM (as a new row of `#inbox`)
+  const messageNew = `
+                <div class="row message unread">
+                  <div class="col-3">${message.sender}</div>
+                  <div class="col-9">${message.subject}</div>
+                </div>
+              `;
+  document.querySelector('#inbox').insertAdjacentHTML('afterbegin', messageNew);
 };
 
+let count = 2;
 const refresh = () => {
   // TODO: Implement the global refresh logic. If there is a new message,
   //       append it to the DOM. Update the unread counter in title as well.
+  if (hasNewMessage()) {
+    appendMessageToDom(newMessage());
+    document.querySelector('#count').innerText = `(${count})`;
+    count++;
+  }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
