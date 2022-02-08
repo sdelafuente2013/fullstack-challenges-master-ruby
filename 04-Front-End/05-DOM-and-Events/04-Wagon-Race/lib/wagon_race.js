d1d6174069f0648590640a8d6ebbc66b31fc19da
santi <santiagodelafuente2013@gmail.com>
2022-02-08 17:06:49 -0300

    Ejercicio 04 terminado

--- 04-Front-End/05-DOM-and-Events/04-Wagon-Race/lib/wagon_race.js
+++ 04-Front-End/05-DOM-and-Events/04-Wagon-Race/lib/wagon_race.js
@@ -1,1 +1,33 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-plusplus */
 // TODO: write your code here
+let movePlayerOne = 2;
+let countPlayerOne = 1;
+
+let movePlayerTwo = 2;
+let countPlayerTwo = 1;
+
+document.addEventListener("keyup", (e) => {
+  if (e.code === 'KeyQ') {
+    if (movePlayerOne === 5) {
+      alert("Win Player One");
+      location.reload();
+    } else {
+      document.querySelector(`#player1-race td:nth-child(${movePlayerOne})`).classList.add('active');
+      document.querySelector(`#player1-race td:nth-child(${countPlayerOne})`).classList.remove('active');
+      movePlayerOne++;
+      countPlayerOne++;
+    }
+  }
+  if (e.code === 'KeyP') {
+    if (movePlayerTwo === 5) {
+      alert("Win Player Two");
+      location.reload();
+    } else {
+      document.querySelector(`#player2-race td:nth-child(${movePlayerTwo})`).classList.add('active');
+      document.querySelector(`#player2-race td:nth-child(${countPlayerTwo})`).classList.remove('active');
+      movePlayerTwo++;
+      countPlayerTwo++;
+    }
+  }
+});
