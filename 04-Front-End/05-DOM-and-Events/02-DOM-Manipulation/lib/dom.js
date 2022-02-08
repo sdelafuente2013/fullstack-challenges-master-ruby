d815e7ee7a53a7b1c73f0d80263684dc2b2645f3
santi <santiagodelafuente2013@gmail.com>
2022-02-08 13:10:57 -0300

    Ejercio 02 terminado

--- 04-Front-End/05-DOM-and-Events/02-DOM-Manipulation/lib/dom.js
+++ 04-Front-End/05-DOM-and-Events/02-DOM-Manipulation/lib/dom.js
@@ -14,27 +14,30 @@ module.exports = function runChallenges(check) {
   //       Make the function getEmail() return it
   const getEmail = () => {
     // TODO: return the email
+    return document.querySelector("#email").value;
   };
 
   // /!\ DO NOT CHANGE THIS LINE - TEST PURPOSES
   const borisesEmail = getEmail();
 
   // Ex 2. Change the content of the email input by writing your own email address
-
+  // getEmail = "santiagodelafuente@gmail.com";
+  document.querySelector("#email").value = "santiagodelafuente@gmail.com";
 
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   //       The text should be emphasized using a <strong> tag
-
-
+  document.querySelector("#email-hint").innerHTML = '<strong>This is my email now</strong>';
 
   // Ex 4. Add the .blue CSS class to the th elements
-
-
+  document.querySelectorAll("th").forEach((item) => {
+    item.classList.add('blue');
+  });
 
   // Ex 5. Count the number of table body rows there are
   //       Make the function teamCount() return it
   const teamCount = () => {
     // TODO: return the number of teams
+    return document.querySelectorAll("tbody tr").length;
   };
 
   // /!\ DO NOT CHANGE THIS LINE - TEST PURPOSES
@@ -42,22 +45,27 @@ module.exports = function runChallenges(check) {
 
   // Ex 6. Say there is a 15th team added to the table.
   //       Add a row at the bottom, this new team should have zero points.
-
-
+  document.querySelector("tbody").insertAdjacentHTML("beforeend", '<tr><td>15</td><td>Santi</td><td>0</td></tr>');
 
   // Ex 7. Write some code to sum all points given to all teams
   //       Make the function summarizePoints() return it
   const summarizePoints = () => {
     // TODO: return the sum
+    let totalPoint = 0;
+    document.querySelectorAll("tbody tr td:nth-child(3)").forEach((item) => {
+      totalPoint += Number.parseInt(item.innerText, 10);
+    });
+    return totalPoint;
   };
 
-
   // Ex 8. Change the background color of all `<th>` cells to #DDF4FF
-
+  document.querySelectorAll('th').forEach((item) => {
+    item.style.backgroundColor = '#DDF4FF';
+  });
 
 
   // Ex 9. Remove the "Email:" label from the DOM
-
+  document.querySelector('label').remove();
 
 
 
